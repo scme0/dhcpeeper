3a8c47f1a86202db59b16a108f95950372ba42f3
scme0 <scott.merchant@octopus.com>
2025-07-20 10:54:38 +0200

    wip

---
+++ go.mod
@@ -0,0 +1,15 @@
+module dhcpeeper
+
+go 1.24
+
+require (
+	github.com/digineo/go-dhclient v1.0.2 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/josharian/native v1.1.0 // indirect
+	github.com/mdlayher/packet v1.1.2 // indirect
+	github.com/mdlayher/raw v0.1.0 // indirect
+	github.com/mdlayher/socket v0.4.1 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/mdlayher/raw"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Printf("syntax: %s [flags] IFNAME\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	//flag.Var(&options, "option", "custom DHCP option for the request (code,value)")
+	//flag.Var(&requestParams, "request", "Additional value for the DHCP Request List Option 55 (code)")
+}
+
+func main() {
+	err := start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func start() (err error) {
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	ifname := flag.Arg(0)
+	iface, err := net.InterfaceByName(ifname)
+	if err != nil {
+		return
+	}
+
+	conn, err := raw.ListenPacket(iface, uint16(layers.EthernetTypeIPv4), nil)
+	if err != nil {
+		return
+	}
+
+	xid := rand.Uint32()
+
+	toSend := newPacket(layers.DHCPMsgTypeInform, iface.HardwareAddr, xid)
+
+	log.Printf("sending packet")
+	err = sendMulticast(conn, toSend, iface.HardwareAddr)
+	if err != nil {
+		return
+	}
+
+	received, err := waitForResponse(conn, xid, 30*time.Second)
+	if err != nil {
+		return
+	}
+
+	if received != nil {
+		log.Printf("received packet %o", received)
+	}
+
+	defer func() {
+		err = conn.Close()
+	}()
+	return
+}
+
+func newPacket(msgType layers.DHCPMsgType, addr net.HardwareAddr, xid uint32) *layers.DHCPv4 {
+	pack := layers.DHCPv4{
+		Operation:    layers.DHCPOpRequest,
+		HardwareType: layers.LinkTypeEthernet,
+		ClientHWAddr: addr,
+		Xid:          xid,
+	}
+
+	pack.Options = append(pack.Options, layers.DHCPOption{
+		Type:   layers.DHCPOptMessageType,
+		Data:   []byte{byte(msgType)},
+		Length: 1,
+	})
+
+	return &pack
+}
+
+func sendMulticast(conn *raw.Conn, dhcp *layers.DHCPv4, addr net.HardwareAddr) error {
+	eth := layers.Ethernet{
+		EthernetType: layers.EthernetTypeIPv4,
+		SrcMAC:       addr,
+		DstMAC:       layers.EthernetBroadcast,
+	}
+	ip := layers.IPv4{
+		Version:  4,
+		TTL:      64,
+		SrcIP:    []byte{0, 0, 0, 0},
+		DstIP:    []byte{255, 255, 255, 255},
+		Protocol: layers.IPProtocolUDP,
+	}
+	udp := layers.UDP{
+		SrcPort: 68,
+		DstPort: 67,
+	}
+
+	// Serialize packet
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		ComputeChecksums: true,
+		FixLengths:       true,
+	}
+	err := udp.SetNetworkLayerForChecksum(&ip)
+	if err != nil {
+		return err
+	}
+	err = gopacket.SerializeLayers(buf, opts, &eth, &ip, &udp, dhcp)
+	if err != nil {
+		return err
+	}
+
+	// Send packet
+	_, err = conn.WriteTo(buf.Bytes(), &raw.Addr{HardwareAddr: eth.DstMAC})
+	return err
+}
+
+// waitForResponse waits for a DHCP packet with matching transaction ID and the given message type
+func waitForResponse(conn *raw.Conn, xid uint32, timeout time.Duration) (*layers.DHCPv4, error) {
+	err := conn.SetReadDeadline(time.Now().Add(timeout))
+	if err != nil {
+		return nil, err
+	}
+	recvBuf := make([]byte, 1500)
+	for {
+		_, _, err = conn.ReadFrom(recvBuf)
+
+		if err != nil {
+			return nil, err
+		}
+
+		pack := parsePacket(recvBuf)
+		if pack == nil {
+			continue
+		}
+
+		if pack.Xid == xid && pack.Operation == layers.DHCPOpReply {
+			return pack, nil
+		}
+	}
+}
+
+// parsePacket decodes a DHCPv4 packet
+func parsePacket(data []byte) *layers.DHCPv4 {
+	pack := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.Default)
+	dhcpLayer := pack.Layer(layers.LayerTypeDHCPv4)
+
+	if dhcpLayer == nil {
+		// received packet is not DHCP
+		return nil
+	}
+	return dhcpLayer.(*layers.DHCPv4)
+}
